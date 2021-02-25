82d1d23b25b4150cf705847aff1e7a1e50cd7306
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-02-25 00:45:24 -0500

    added functions to get languages and proficiencies

--- src/api/api.js
+++ src/api/api.js
@@ -23,3 +23,31 @@ export const getRaces = async () => {
 
     return result;
 }
+
+export const getProficiencies = async (classType) => {
+    const results = await axios.get(`http://www.dnd5eapi.co/api/classes/${classType}`)
+        .then(res => {
+            console.log(res.data.proficiencies);
+            return res.data.proficiencies;
+
+        })
+        .catch(err => {
+            return err;
+        })
+
+    return results;
+}
+
+export const getLanguages = async (race) => {
+    const results = await axios.get(`http://www.dnd5eapi.co/api/races/${race}`)
+        .then(res => {
+            console.log(res.data.languages);
+            return res.data.languages;
+
+        })
+        .catch(err => {
+            return err;
+        })
+
+    return results;
+}
